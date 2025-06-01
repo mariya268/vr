81142016dccf8e049c6e8ba54856b448fb4b6df3
mariya268 <masha7514@gmail.com>
2025-06-01 17:43:27 +0300

    AR projectt

---
+++ ARm/Assets/ssss.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ARController : MonoBehaviour
+{
+    [SerializeField] private Animator solderingStationAnimator; // Ссылка на Animator модели
+    [SerializeField] private string animationTriggerName = "StartAnimation"; // Имя триггера анимации
+
+    private void Update()
+    {
+        // Проверяем нажатие левой кнопки мыши
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlaySolderingStationAnimation();
+        }
+    }
+
+    private void PlaySolderingStationAnimation()
+    {
+        // Проверяем, что аниматор присвоен
+        if (solderingStationAnimator != null)
+        {
+            // Активируем триггер для запуска анимации
+            solderingStationAnimator.SetTrigger(animationTriggerName);
+        }
+        else
+        {
+            Debug.LogWarning("Animator для паяльной станции не присвоен!");
+        }
+    }
+}
